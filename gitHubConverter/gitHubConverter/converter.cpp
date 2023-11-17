843a9058953ff06a59797e5bc3f626f962e58e65
(Student  C00296873) Aoife Leahy <C00296873@setu.ie>
2023-11-17 10:29:39 +0000

    second in command temp done

--- gitHubConverter/gitHubConverter/converter.cpp
+++ gitHubConverter/gitHubConverter/converter.cpp
@@ -5,6 +5,8 @@
 
 char printMenu(); // function prototype. 
 float degreesToRadians(float userInput); // Leader first conversion prototype
+float fahrenheitToCelsius(float userInput); // written by second in command
+
 
 int main()
 {
@@ -12,12 +14,20 @@ int main()
     menuOption = printMenu();// holds user option from the menu function 
     float userInput = 0.0f; // holds what the user wants to convert
     float answer = 0.0f; // holds the output for any of the conversions 
-    if (menuOption = 'A')
+    if (menuOption == 'A')
     {
         std::cout << "You would like to convert: from degrees to radians. \n \n" << std::endl; 
         std::cout << "Please enter your measurement in degrees: "; 
         std::cin >> userInput;
         answer = degreesToRadians(userInput);
+        std::cout << "Your measurement is now: " << answer << std::endl;
+    }
+    if (menuOption == 'f')
+    {
+        std::cout << "You would like to convert: fahrenheit to celsius. \n \n" << std::endl;
+        std::cout << "Please enter your measurement in fahrenheit: ";
+        std::cin >> userInput;
+        answer = fahrenheitToCelsius(userInput);
         std::cout << "Your measurement is now: " << answer << std::endl;
     }
 	return 1;
@@ -39,11 +49,20 @@ char printMenu() // written by leader
     return choice;
 }
 
-float degreesToRadians(float userInput)
+float degreesToRadians(float t_userInput)
 {
     float answer = 0.0f; 
     const float CONVERSION_RATE = 57.2957795; 
-    answer = userInput / CONVERSION_RATE; // conversion formula from google 
+    answer = t_userInput / CONVERSION_RATE; // conversion formula from google 
 
     return answer;
+}
+
+float fahrenheitToCelsius(float t_userInput)
+{
+    float answer = 0.0f;
+    answer = (t_userInput - 32 / 1.8);
+
+    return answer;
+
 }
